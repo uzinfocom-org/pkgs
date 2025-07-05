8cae4f8b5d33f31d81dfce3dda13111be1263c5f
bahrom04 <magdiyevbahrom@gmail.com>
2025-07-05 20:17:31 +0500

    chore: single mkUser iterator for mkUsers

--- lib/users.nix
+++ lib/users.nix
@@ -1,38 +1,42 @@
 {lib}: let
-  mkUsers = users: upstream: let
-    inputs = upstream.inputs;
-    outputs = upstream.outputs;
-  in {
+  mkUser = i: {
+    name = i.username;
+    value = {
+      hashedPassword = "${i.hashedPassword}";
+      isNormalUser = true;
+      description = "${i.description}";
+
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "docker"
+        "admins"
+      ];
+
+      openssh.authorizedKeys.keys =
+        lib.optionals # function
+        
+        ((i.githubKeysUrl != "") && (i.sha256 != "")) # arg1
+        
+        (lib.strings.splitString
+          "\n" (builtins.readFile (builtins.fetchurl {
+            url = "${i.githubKeysUrl}";
+            sha256 = "${i.sha256}";
+          })))
+        # arg2
+        ;
+    };
+  };
+  mkUsers = users: upstream: {
     # mapped users
-    users.users = builtins.listToAttrs (builtins.map (i: {
-        name = i.username;
-        value = {
-          hashedPassword = "${i.hashedPassword}";
-          isNormalUser = true;
-          description = "${i.description}";
-
-          extraGroups = [
-            "networkmanager"
-            "wheel"
-            "docker"
-            "admins"
-          ];
-
-          openssh.authorizedKeys.keys =
-            lib.optionals # function
-            ((i.githubKeysUrl != "") && (i.sha256 != "")) # arg1
-            (lib.strings.splitString 
-            "\n" (builtins.readFile (builtins.fetchurl {url = "${i.githubKeysUrl}"; sha256 = "${i.sha256}";}))) # arg2
-          ;
-        };
-      })
-      users);
+    users.users = builtins.listToAttrs (builtins.map mkUser users);
 
     home-manager = {
       backupFileExtension = "hbak";
 
       extraSpecialArgs = {
-        inherit inputs outputs;
+        inputs = upstream.inputs;
+        outputs = upstream.outputs;
       };
       # mapped home-manager
       users = builtins.listToAttrs (builtins.map (i: {
